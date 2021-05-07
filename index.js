2f9e96a629fa98cf52c2fb05c9f403519ccef08a
karen chehade <karen_chehade@hotmail.com>
2021-05-07 00:38:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your Circle class here
+class Circle{
+  constructor(radius){
+    this.radius =radius;
+  }
+  get diameter() {
+    return this.radius * 2;
+  }
+  get circumference() {
+    return `${this.name} says ${this._phrase || 'squawk'}`;
+  }
+  get area() {
+    return `${this.name} says ${this._phrase || 'squawk'}`;
+  }
+  set diameter() {
+   this._phrase = phrase;
+ }
+ set circumference() {
+  this._phrase = phrase;
+}
+set area() {
+ this._phrase = phrase;
+}
+
+}
